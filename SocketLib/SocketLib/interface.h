2cd5b3e1f3a88aeffa0aec124a4c060d8848d054
KazumaOhura <k.ohura0303@gmail.com>
2020-12-05 21:37:00 +0900

    [#5] :sparkles: インターフェースの作成

---
+++ SocketLib/SocketLib/interface.h
@@ -0,0 +1,46 @@
+#ifndef INTERFACE_H_
+
+namespace Network{
+
+//! interface
+class ITcpSock {
+public: 
+	ITcpSock(int port, int address, int family){
+		Initialize(port, address, family);
+	};
+
+	virtual ~ITcpSock(){
+		Finalize();
+	};
+
+	virtual int Initialize(int port, int address, int family) = 0;
+	virtual int Finalize() = 0;
+	virtual int Accept() = 0;
+	virtual int Bind() = 0;
+	virtual int Close() = 0;
+	virtual int Listen() = 0;
+	virtual int Recieve() = 0;
+	virtual int Send() = 0;
+};
+
+//! interface
+class IUdpSock {
+public:
+	IUdpSock(int port, int address, int family){
+		Initialize(port, address, family);
+	};
+
+	virtual ~IUdpSock(){
+		Finalize();
+	};
+
+	virtual int Initialize(int port, int address, int family) = 0;
+	virtual int Finalize() = 0;
+	virtual int Bind() = 0;
+	virtual int Close() = 0;
+	virtual int Recieve() = 0;
+	virtual int Send() = 0;
+};
+}
+
+#endif //! INTERFACE_H_
