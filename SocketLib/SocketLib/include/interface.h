2a7ee4a5f12ebd59b1ac52ca2b4356b4a695657c
KazumaOhura <k.ohura0303@gmail.com>
2020-12-07 01:17:39 +0900

    [#13]:sparkles:SocketInfoクラスを追加

---
+++ SocketLib/SocketLib/include/interface.h
@@ -0,0 +1,43 @@
+#ifndef INTERFACE_H_
+#define INTERFACE_H_
+
+#include <string>
+
+namespace network{
+
+using Port = unsigned short;
+using Address = std::string;
+using Family = int;
+
+//! interface
+class ITcpSock {
+public: 
+	ITcpSock(){}
+	virtual ~ITcpSock(){}
+
+	virtual int Initialize(const Address& address, const Port& port, const Family& family)const = 0;
+	virtual int Finalize()const = 0;
+	virtual int Accept()const = 0;
+	virtual int Bind()const = 0;
+	virtual int Close()const = 0;
+	virtual int Listen()const = 0;
+	virtual int Recieve(char* buffer)const = 0;
+	virtual int Send(char* buffer)const = 0;
+};
+
+//! interface
+class IUdpSock {
+public:
+	IUdpSock(){}
+	virtual ~IUdpSock(){}
+
+	virtual int Initialize(const Address& address, const Port& port, const Family& family) = 0;
+	virtual int Finalize()const = 0;
+	virtual int Bind()const = 0;
+	virtual int Close()const = 0;
+	virtual int Recieve(char* buffer)const = 0;
+	virtual int Send(char* buffer)const = 0;
+};
+}
+
+#endif //! INTERFACE_H_
