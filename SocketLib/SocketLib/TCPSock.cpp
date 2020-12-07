e8c24351fdad07c5191e0390f3ab6cc06a2ec880
tamaichihiro <mikumarumutyo@gmail.com>
2020-12-07 23:57:21 +0900

    namespace内のインデントを削除

--- SocketLib/SocketLib/TCPSock.cpp
+++ SocketLib/SocketLib/TCPSock.cpp
@@ -3,92 +3,92 @@
 namespace network{
 
 
-	int TcpSock::Initialize(const Address& address, const Port& port, const Family& family)const {
-		WSAStartup(MAKEWORD(2, 0), &this->m_socket_info->wsadata);
+int TcpSock::Initialize(const Address& address, const Port& port, const Family& family)const {
+	WSAStartup(MAKEWORD(2, 0), &this->m_socket_info->wsadata);
 
-		memset(&this->m_socket_info->mysockaddr, 0, sizeof(MySockaddr));
+	memset(&this->m_socket_info->mysockaddr, 0, sizeof(MySockaddr));
 
-		this->InitSocket(address, port, family);
+	this->InitSocket(address, port, family);
 
-		this->m_socket_info->socket = socket(family, SOCK_STREAM, 0);
+	this->m_socket_info->socket = socket(family, SOCK_STREAM, 0);
 
-		return 0;
+	return 0;
 
+}
+
+int TcpSock::InitSocket(const Address& address, const Port& port, const Family& family)const {
+
+	this->m_socket_info->mysockaddr.sin_port = htons(port);
+	this->m_socket_info->mysockaddr.sin_family = family;
+	inet_pton(this->m_socket_info->mysockaddr.sin_family,address.c_str(),&this->m_socket_info->mysockaddr.sin_addr.S_un.S_addr);
+
+	return 0;
+
+}
+
+int TcpSock::Bind()const {
+
+	bind(this->m_socket_info->socket, (struct sockaddr*)&this->m_socket_info->mysockaddr, sizeof(this->m_socket_info->mysockaddr));
+
+	return 0;
+
+}
+
+int TcpSock::Listen(int listen_time)const {
+
+	listen(this->m_socket_info->socket, listen_time);
+
+	return 0;
+
+}
+
+int TcpSock::Accept() {
+
+	this->oppnent_len = sizeof(this->m_oppnent);
+
+	this->m_oppnent_sock.reset((SOCKET*)accept(this->m_socket_info->socket, (struct sockaddr*)&this->m_oppnent, &this->oppnent_len));
+
+	if (this->m_socket_info->socket == INVALID_SOCKET) {
+		printf("accept : %d\n", WSAGetLastError());
+		return 1;
 	}
+	return 0;
 
-	int TcpSock::InitSocket(const Address& address, const Port& port, const Family& family)const {
+}
 
-		this->m_socket_info->mysockaddr.sin_port = htons(port);
-		this->m_socket_info->mysockaddr.sin_family = family;
-		inet_pton(this->m_socket_info->mysockaddr.sin_family,address.c_str(),&this->m_socket_info->mysockaddr.sin_addr.S_un.S_addr);
+int TcpSock::Send(char* buffer)const {
 
-		return 0;
+	send(this->m_socket_info->socket, buffer, sizeof(buffer), 0);
 
-	}
+	return 0;
 
-	int TcpSock::Bind()const {
+}
 
-		bind(this->m_socket_info->socket, (struct sockaddr*)&this->m_socket_info->mysockaddr, sizeof(this->m_socket_info->mysockaddr));
+int TcpSock::Recieve(char* buffer)const {
 
-		return 0;
+	memset(buffer, 0, sizeof(buffer));
 
-	}
+	recv(this->m_socket_info->socket, buffer, sizeof(buffer), 0);
 
-	int TcpSock::Listen(int listen_time)const {
+	return 0;
 
-		listen(this->m_socket_info->socket, listen_time);
+}
 
-		return 0;
+int TcpSock::Close()const {
 
-	}
+	closesocket(this->m_socket_info->socket);
 
-	int TcpSock::Accept() {
+	return 0;
 
-		this->oppnent_len = sizeof(this->m_oppnent);
+}
 
-		this->m_oppnent_sock.reset((SOCKET*)accept(this->m_socket_info->socket, (struct sockaddr*)&this->m_oppnent, &this->oppnent_len));
+int TcpSock::Finalize()const {
 
-		if (this->m_socket_info->socket == INVALID_SOCKET) {
-			printf("accept : %d\n", WSAGetLastError());
-			return 1;
-		}
-		return 0;
+	Close();
 
-	}
+	WSACleanup();
 
-	int TcpSock::Send(char* buffer)const {
+	return 0;
 
-		send(this->m_socket_info->socket, buffer, sizeof(buffer), 0);
-
-		return 0;
-
-	}
-
-	int TcpSock::Recieve(char* buffer)const {
-
-		memset(buffer, 0, sizeof(buffer));
-
-		recv(this->m_socket_info->socket, buffer, sizeof(buffer), 0);
-
-		return 0;
-
-	}
-
-	int TcpSock::Close()const {
-
-		closesocket(this->m_socket_info->socket);
-
-		return 0;
-
-	}
-
-	int TcpSock::Finalize()const {
-
-		Close();
-
-		WSACleanup();
-
-		return 0;
-
-	}
+}
 }
--- SocketLib/SocketLib/TCPSock.h
+++ SocketLib/SocketLib/TCPSock.h
@@ -8,33 +8,33 @@
 #include "include/socket_info.h"
 
 namespace network{
-	class TcpSock : public ITcpSock {
-	public:
+class TcpSock : public ITcpSock {
+public:
 
-		TcpSock(const Address& address, const Port& port, const Family& family) {
-			Initialize(address, port, family);
-		};
-
-		~TcpSock() {
-			Finalize();
-		};
-
-		virtual int Initialize(const Address& address, const Port& port, const Family& family)const override;
-		virtual int Finalize()const override;
-		virtual int Accept()override;
-		virtual int Bind()const override;
-		virtual int Close()const override;
-		virtual int Listen(int listen_time)const override;
-		virtual int Recieve(char* buffer)const override;
-		virtual int Send(char* buffer)const override;
-	private:
-		std::unique_ptr<SocketInfo> m_socket_info;
-		std::unique_ptr<sockaddr_in> m_oppnent;
-		std::unique_ptr<SOCKET> m_oppnent_sock;
-		int oppnent_len;
-
-		int InitSocket(const Address& address, const Port& port, const Family& family)const;
+	TcpSock(const Address& address, const Port& port, const Family& family) {
+		Initialize(address, port, family);
 	};
+
+	~TcpSock() {
+		Finalize();
+	};
+
+	virtual int Initialize(const Address& address, const Port& port, const Family& family)const override;
+	virtual int Finalize()const override;
+	virtual int Accept()override;
+	virtual int Bind()const override;
+	virtual int Close()const override;
+	virtual int Listen(int listen_time)const override;
+	virtual int Recieve(char* buffer)const override;
+	virtual int Send(char* buffer)const override;
+private:
+	std::unique_ptr<SocketInfo> m_socket_info;
+	std::unique_ptr<sockaddr_in> m_oppnent;
+	std::unique_ptr<SOCKET> m_oppnent_sock;
+	int oppnent_len;
+
+	int InitSocket(const Address& address, const Port& port, const Family& family)const;
+};
 
 }
 #endif // !TCP_H
