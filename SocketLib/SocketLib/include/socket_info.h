21cd1038a3c7da2ecf5fe81a25d3e482e1ed7e63
KazumaOhura <k.ohura0303@gmail.com>
2020-12-10 00:49:39 +0900

    [#7]:sparkles:必要な情報をSocketInfo型に追加

--- SocketLib/SocketLib/include/socket_info.h
+++ SocketLib/SocketLib/include/socket_info.h
@@ -6,12 +6,17 @@
 #pragma comment(lib, "ws2_32.lib")
 
 using MySockaddr = sockaddr_in;
+using OpponentSockaddr = sockaddr_in;
+using Size = int;
 
 namespace network {
 struct SocketInfo {
-	WSADATA			wsadata;
-	SOCKET			socket;
-	MySockaddr		mysockaddr;
+	WSADATA				wsadata;
+	SOCKET				socket;
+	MySockaddr			my_sockaddr;
+	Size				my_sockaddr_size;
+	OpponentSockaddr	oppnent_sockaddr;
+	Size				oppnent_sockaddr_size;
 };
 }
 
