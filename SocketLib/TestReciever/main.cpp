64ea4a264b412a036dd2ffb80541b0d53c87c2d8
KazumaOhura <k.ohura0303@gmail.com>
2020-12-12 05:46:17 +0900

    [#7] :rotating_light: 自作の型においても通信が可能なことを確認

--- SocketLib/TestReciever/main.cpp
+++ SocketLib/TestReciever/main.cpp
@@ -4,21 +4,30 @@
 
 #include "../SocketLib/include/udp.h"
 
+struct TEST {
+	int a;
+	int b;
+	int c;
+};
+
 int main() {
 
 	std::unique_ptr<network::IUdpSock> sock = std::make_unique<network::UdpSock>("0.0.0.0", 50000, AF_INET);
 
+	TEST test;
 	char buffer[256] = {0};
 
 	sock->Bind();
 
 	while (1) {
-		sock->Recieve(buffer, sizeof(buffer));
-		if (buffer) {
-			std::cout << std::string(buffer) << std::endl;
+		sock->Recieve(reinterpret_cast<char*>(&test), sizeof(test));
+		if (test.a) {
+			std::cout << std::string(std::to_string(test.a)) << std::endl;
 		}
 		//Sleep(100);
 	}
+
+	getchar();
 
 	return 0;
 }
--- SocketLib/TestSender/main.cpp
+++ SocketLib/TestSender/main.cpp
@@ -5,17 +5,26 @@
 
 #include "../SocketLib/include/udp.h"
 
+struct TEST {
+	int a = 53;
+	int b = 42;
+	int c = 18;
+};
+
 int main() {
 
 	std::unique_ptr<network::IUdpSock> sock = std::make_unique<network::UdpSock>("127.0.0.1", 50000, AF_INET);
 	char message[256];
+	TEST test;
 	memset(message, 0, sizeof(message));
 
+	/*
 	std::cin >> message;
 	OutputDebugStringA(message);
 	OutputDebugStringA("\n");
+	*/
 
-	sock->Send("127.0.0.1", 50000, message, strlen(message));
+	sock->Send("127.0.0.1", 50000, reinterpret_cast<const char*>(&test), sizeof(test));
 
 	getchar();
 
