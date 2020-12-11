7c7ffda16fd2c3231d1e4146efbe0974801802ca
tamaichihiro <mikumarumutyo@gmail.com>
2020-12-12 00:16:35 +0900

    :sparkles: :rotating_light: 送信側のテストコードがなかったので追加しました。

--- SocketLib/SocketLib/src/main.cpp
+++ SocketLib/SocketLib/src/main.cpp
@@ -7,18 +7,33 @@
 
 
 int main() {
-
+	//óM‘¤
+	/*
 	std::unique_ptr<network::ITcpSock> sock = std::make_unique<network::TcpSock>("127.0.0.1", 12345, AF_INET);
 	char buffer[32];
 
 	memset(&buffer, 0, sizeof(buffer));
-
+	
 	sock->Connect();
 
 	sock->Recieve(buffer);
 
-		std::cout << buffer << std::endl;
-	
+	std::cout << buffer << std::endl;
+	*/
+
+	//‘—M‘¤
+	std::unique_ptr<network::ITcpSock> sock = std::make_unique<network::TcpSock>("0.0.0.0", 12345, AF_INET);
+	char buffer[32] = { "abcde" };
+	int time = 5;
+
+
+	sock->Bind();
+
+	sock->Listen(time);
+
+	sock->Accept();
+
+	sock->Send(buffer);
 
 	sock->Close();
 }
